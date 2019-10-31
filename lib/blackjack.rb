01cc6673ea0c574697db3c6fa436a38def63473b
mwong068 <mwong068@gmail.com>
2019-10-30 17:36:10 -0700

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
+# require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  total
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp()
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = 0
+  2.times {sum += deal_card}
+  display_card_total(sum)
+  sum
 end
 
-def hit?
-  # code hit? here
+def hit?(sum)
+  prompt_user
+  user_input = get_user_input
+     if user_input == 's'
+      sum
+    elsif user_input == 'h'
+      sum += deal_card
+    else
+      invalid_command
+      prompt_user
+      get_user_input
+    end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +55,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  sum = initial_round
+  new_sum = hit?(sum)
+  display_card_total(new_sum)
+  if new_sum > 21
+    end_game(new_sum)
+  end
+
 end
+
+# binding.pry
     
